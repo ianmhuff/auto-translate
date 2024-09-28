31c02dafbfe9cab44d23f76fac6828792c5748aa
ianmhuff <ianmhuff@gmail.com>
2024-09-27 22:12:23 -0400

    v0.4
    return_value_xx, more operators, file extension checking

--- auto-translate.py
+++ auto-translate.py
@@ -1,123 +1,150 @@
 # auto-translate.py
-# v0.3
+# v0.4
 
 # ideas/todo:
-# deal with return_value_xx lines
-# operators other than = (==, +, -, *, <, >, <=, >=, etc.) (also operator.cast.to.bool ?)
+# operator.cast.to.bool ?
 # other as_[type]() functions (currently handling as_integer() and as_hash())
 # _SITUATION... -> *SITUATION... (also _FIGHTER..., _GROUND..., etc)
-#   this seems difficult to do comprehensively without a complete list of cases where this happens
+#     this seems difficult to do comprehensively without a complete list of cases where this happens
 # returns? i.e. return_value = 0; -> return 0.into();
-#   this one would need to be at the bottom since it depends on the formatting
+#     this one would need to be at the bottom since it depends on the formatting
 
 
 import os
 import re
 
 def translate():
-    # Get the current directory
     current_directory = os.getcwd()
-
-    # Iterate through all files in the directory
+    valid_extensions = [".txt", ".c", ".rs"]
     for filename in os.listdir(current_directory):
     
-        #don't modify the script file lmao
+        # don't modify the script file lmao
         if filename == os.path.basename(__file__):
+            continue
+        
+        # only modify files of certain types
+        ext = os.path.splitext(filename)[1]
+        if ext not in valid_extensions:
             continue
         
         file_path = os.path.join(current_directory, filename)
 
-        # Only process files (not directories)
         if os.path.isfile(file_path):
             with open(file_path, 'r') as file:
                 lines = file.readlines()
             
-            # Create a new list to hold modified lines
             modified_lines = []
             
             for line in lines:                
                 
+                
+                # ======= removals =======
+                
                 # remove "(L2CValue *)&" from all lines
-                substring = "(L2CValue *)&"
-                if substring in line:
-                    line = line.replace(substring, "")
+                pattern = "(L2CValue *)&"
+                if pattern in line:
+                    line = line.replace(pattern, "")
                 
                 
                 # remove "(L2CValue *)" from all lines
-                substring = "(L2CValue *)"
-                if substring in line:
-                    line = line.replace(substring, "")
+                pattern = "(L2CValue *)"
+                if pattern in line:
+                    line = line.replace(pattern, "")
                 
                 
                 # remove "app::lua_bind::" from all lines
-                substring = "app::lua_bind::"
-                if substring in line:
-                    line = line.replace(substring, "")
+                pattern = "app::lua_bind::"
+                if pattern in line:
+                    line = line.replace(pattern, "")
                 
                 
                 # remove "_impl" from all lines
-                substring = "_impl"
-                if substring in line:
-                    line = line.replace(substring, "")
+                pattern = "_impl"
+                if pattern in line:
+                    line = line.replace(pattern, "")
                 
+                
+                # remove "return_value_xx" from all lines
+                pattern = r",return_value_\d+"
+                match = re.search(pattern, line)
+                if match:
+                    line = re.sub(pattern, "", line)
+                
+                
+                
+                
+                # ======= replacements =======
                 
                 # replace __
                 # with ::
-                substring = "__"
-                if substring in line:
-                    line = line.replace(substring, "::")
-                
-                
-                # reformat lib::L2CValue::~L2CValue(var1);
-                # to //free(var1);
-                pattern = r"(\s*)lib::L2CValue::~L2CValue\((.+?)\);\n"
-                match = re.fullmatch(pattern, line)
-                if match:
-                    print(match)
-                    whitespace, token1 = match.groups()
-                    line = f"{whitespace}//free({token1});\n"
+                pattern = "__"
+                if pattern in line and "thiscall" not in line:
+                    line = line.replace(pattern, "::")
                 
                 
                 # replace this->moduleAccessor
                 # with fighter.module_accessor
-                substring = "this->moduleAccessor"
-                if substring in line:
-                    line = line.replace(substring, "fighter.module_accessor")
+                pattern = "this->moduleAccessor"
+                if pattern in line:
+                    line = line.replace(pattern, "fighter.module_accessor")
                 
                 
                 # replace this->globalTable
                 # with fighter.global_table
-                substring = "this->globalTable"
-                if substring in line:
-                    line = line.replace(substring, "fighter.global_table")
+                pattern = "this->globalTable"
+                if pattern in line:
+                    line = line.replace(pattern, "fighter.global_table")
                 
                 
-                # reformat lib::L2CValue::L2CValue(var1,var2);
-                # to var1 = var2;
-                pattern = r"(\s*)lib::L2CValue::L2CValue\((.+?),\s*(.+?)\);\n"
+                
+                
+                # ======= operators =======
+                
+                # reformat lib::L2CValue::operator[op](var1,var2,var3);
+                # to var3 = var1 [op] var2;
+                pattern = r"(\s*)lib::L2CValue::operator(.*)\((.+?),(.+?),(.+?)\);\n"
                 match = re.fullmatch(pattern, line)
                 if match:
-                    print(match)
-                    whitespace, token1, token2 = match.groups()
-                    line = f"{whitespace}{token1} = {token2};\n"
+                    whitespace, op, token1, token2, token3 = match.groups()
+                    line = f"{whitespace}{token3} = {token1} {op} {token2};\n"
                 
                 
                 # reformat lib::L2CValue::operator=(var1,var2);
                 # to var1 = var2;
-                pattern = r"(\s*)lib::L2CValue::operator=\((.+?),\s*(.+?)\);\n"
+                pattern = r"(\s*)lib::L2CValue::operator=\((.+?),(.+?)\);\n"
                 match = re.fullmatch(pattern, line)
                 if match:
-                    print(match)
                     whitespace, token1, token2 = match.groups()
                     line = f"{whitespace}{token1} = {token2};\n"
                 
+                
+                # reformat lib::L2CValue::operator[](var1,var2)
+                # to var1[var2]
+                pattern = r"(\s*)(.+?)lib::L2CValue::operator\[\]\((.+?),(.+?)\);\n"
+                match = re.search(pattern, line)
+                if match:
+                    whitespace, other, token1, token2 = match.groups()
+                    line = f"{whitespace}{other}{token1}[{token2}];\n"
+                
+                
+                # reformat lib::L2CValue::operator[op](var1,var2);
+                # to var1 [op] var2;
+                pattern = r"(\s*)(.+?)lib::L2CValue::operator(.*)\((.+?),(.+?)\);\n"
+                match = re.search(pattern, line)
+                if match:
+                    whitespace, other, op, token1, token2 = match.groups()
+                    line = f"{whitespace}{other}{token1} {op} {token2};\n"
+                
+                
+                
+                
+                # ======= if statements =======
                 
                 # reformat if ((var1 & 1) != 0)
                 # to if var1
                 pattern = r"(\s*)if \(\((.+?) & 1\) != 0\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, token1, other = match.groups()
                     line = f"{whitespace}if {token1}{other}\n"
                 
@@ -127,7 +154,6 @@ def translate():
                 pattern = r"(\s*)if \(\((.+?) & 1\) == 0\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, token1, other = match.groups()
                     line = f"{whitespace}if !{token1}{other}\n"
                 
@@ -137,7 +163,6 @@ def translate():
                 pattern = r"(\s*)if \(\((.+?) & 1U\) != 0\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, token1, other = match.groups()
                     line = f"{whitespace}if {token1}{other}\n"
                 
@@ -147,27 +172,20 @@ def translate():
                 pattern = r"(\s*)if \(\((.+?) & 1U\) == 0\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, token1, other = match.groups()
                     line = f"{whitespace}if !{token1}{other}\n"
                 
                 
-                # reformat (bool)(var1 & 1)
-                # to var1
-                pattern = r"(\s*)(.*)\(bool\)\((.+?) & 1\)(.*)"
-                match = re.search(pattern, line)
-                if match:
-                    print(match)
-                    whitespace, other1, token1, other2 = match.groups()
-                    line = f"{whitespace}{other1}{token1}{other2}\n"
                 
+                
+                
+                # ======= as_type =======
                 
                 # reformat lib::L2CValue::as_integer(var1)
                 # to var1
                 pattern = r"(\s*)(.*)lib::L2CValue::as_integer\((.+?)\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, other1, token1, other2 = match.groups()
                     line = f"{whitespace}{other1}{token1}{other2}\n"
                 
@@ -177,9 +195,39 @@ def translate():
                 pattern = r"(\s*)(.*)lib::L2CValue::as_hash\((.+?)\)(.*)"
                 match = re.search(pattern, line)
                 if match:
-                    print(match)
                     whitespace, other1, token1, other2 = match.groups()
                     line = f"{whitespace}{other1}{token1}{other2}\n"
+                
+                
+                
+                
+                # ======= misc =======
+                
+                # reformat (bool)(var1 & 1)
+                # to var1
+                pattern = r"(\s*)(.*)\(bool\)\((.+?) & 1\)(.*)"
+                match = re.search(pattern, line)
+                if match:
+                    whitespace, other1, token1, other2 = match.groups()
+                    line = f"{whitespace}{other1}{token1}{other2}\n"
+                
+                
+                # reformat lib::L2CValue::L2CValue(var1,var2);
+                # to var1 = var2;
+                pattern = r"(\s*)lib::L2CValue::L2CValue\((.+?),\s*(.+?)\);\n"
+                match = re.fullmatch(pattern, line)
+                if match:
+                    whitespace, token1, token2 = match.groups()
+                    line = f"{whitespace}{token1} = {token2};\n"
+                
+                
+                # reformat lib::L2CValue::~L2CValue(var1);
+                # to //free(var1);
+                pattern = r"(\s*)lib::L2CValue::~L2CValue\((.+?)\);\n"
+                match = re.fullmatch(pattern, line)
+                if match:
+                    whitespace, token1 = match.groups()
+                    line = f"{whitespace}//free({token1});\n"
                 
                 
                 modified_lines.append(line)  
